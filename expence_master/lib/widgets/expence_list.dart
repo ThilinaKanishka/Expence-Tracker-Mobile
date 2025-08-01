ff4fab08e52497d8aea6d33ee0e2a4e4fd471a95
ThilinaKanishka <thilina20010313@gmail.com>
2025-08-01 14:28:11 +0530

    Update expence_list.dart

--- expence_master/lib/widgets/expence_list.dart
+++ expence_master/lib/widgets/expence_list.dart
@@ -1,7 +1,10 @@
+import 'package:expence_master/models/expence.dart';
 import 'package:flutter/material.dart';
 
 class ExpenceList extends StatelessWidget {
-  const ExpenceList({super.key});
+  const ExpenceList({super.key, required this.expenceList});
+
+  final List<ExpenceModel> expenceList;
 
   @override
   Widget build(BuildContext context) {
